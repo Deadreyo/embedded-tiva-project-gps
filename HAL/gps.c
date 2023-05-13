b15227964f7ead55d7ffb897da0ebee2cadbdbc3
tasneem-elhady <127930144+tasneem-elhady@users.noreply.github.com>
2023-05-14 00:40:31 +0300

    Update gps.c

--- HAL/gps.c
+++ HAL/gps.c
@@ -31,3 +31,55 @@ void get_RMC_string()
 	}while(recievedChar != '*');
 }
 
+
+geographic_point get_geographic_point()
+{
+//		char lat[30] = "";
+//    char lon[30] = "";
+	char lat_dir[] = "";
+	char lon_dir[] = "";
+    char valid[] = "";
+		geographic_point p;
+		char *token ;
+
+		get_RMC_string();
+
+		token = strtok(RMC_STRING, ",");
+		token= strtok (NULL,",");
+				strcpy(valid,token);
+		if (strcmp(valid, "A")==0)
+		{
+			p.Valid = 1;
+
+		token= strtok (NULL,",");
+				strcpy(p.lat_str,token);
+
+		token= strtok (NULL,",");
+				strcpy(lat_dir,token);
+			
+		token= strtok (NULL,",");
+				strcpy(p.lon_str,token);
+
+		token= strtok (NULL,",");
+				strcpy(lon_dir,token);
+
+
+
+		if (lat_dir[0] == 'N')
+			p.latitude = str_to_float (p.lat_str);
+		else
+			p.latitude = -str_to_float (p.lat_str);
+
+		if (lon_dir[0] == 'E')
+			p.longtude = str_to_float (p.lon_str);
+		else
+			p.longtude =-str_to_float (p.lon_str);
+		}
+		
+		else
+			p.Valid = 0;
+		
+    return p;
+}
+
+
