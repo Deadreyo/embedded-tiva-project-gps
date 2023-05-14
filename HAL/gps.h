dd6fc47fc0a023346eb7c5085dd58ede38fb6a06
tasneem-elhady <127930144+tasneem-elhady@users.noreply.github.com>
2023-05-14 12:32:59 +0300

    Update gps.h

--- HAL/gps.h
+++ HAL/gps.h
@@ -1,11 +1,18 @@
 #ifndef GPS_H
 #define GPS_H
 
-
-
-
+typedef struct
+{
+	  _Bool Valid;
+    double latitude;
+    double longtude;
+    char lat_str[30] ;
+    char lon_str[30] ;
+} geographic_point;
 
 void get_RMC_string(void);
+
+geographic_point get_geographic_point(void);
 
 
 #endif
