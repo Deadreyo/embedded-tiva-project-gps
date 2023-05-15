bfc360533fc359a29e7f30f96b25cae5dd087d58
Gehad220 <88938459+Gehad220@users.noreply.github.com>
2023-05-15 10:22:48 +0300

    Update timer.c

--- MCAL/timer.c
+++ MCAL/timer.c
@@ -7,20 +7,29 @@
 
 void SysTick_Init(void){
     NVIC_ST_CTRL_R = 0;                 // Disable systick timer
-    NVIC_ST_RELOAD_R = 12-1;        // Load 80 counts to reload register for 1 microsecond count
-    NVIC_ST_CURRENT_R = 0;          // Clear the counter
+    NVIC_ST_RELOAD_R = 0xFFFFFF;
+    NVIC_ST_CURRENT_R = 0;              // Clear the counter
     NVIC_ST_CTRL_R = 5;                 // Enable the counter and system CLK
 }
 
-void delay_ms(long long ms){
-    long long i = ms * 1000;
-    while(i--){
-        delay_us(1);        // 1 us                             // loop for count milliseconds
+
+void delay_ms(long long ms) {
+    while(ms--){
+        NVIC_ST_CTRL_R = 0x05;
+
+        NVIC_ST_RELOAD_R = 16e6 / 1000;
+
+        while( (NVIC_ST_CTRL_R & 0x10000) == 0);
     }
 }
 
+
 void delay_us(long long us){
     while(us--){
-        while((NVIC_ST_CTRL_R&0x10000)==0){}        // Loop while count flag is down
+        NVIC_ST_CTRL_R = 0x05;
+
+        NVIC_ST_RELOAD_R = 16e6 / 1000 / 1000;
+
+        while( (NVIC_ST_CTRL_R & 0x10000) == 0);
     }
 }
